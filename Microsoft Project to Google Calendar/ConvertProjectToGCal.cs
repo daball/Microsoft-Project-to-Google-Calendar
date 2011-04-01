1e2ab9c0c4ed61b1e2c46512bd754faf658e919b
David Ball <david@daball.me>
2011-04-01 06:54:36 -0400

    change platform target to reference system.web.dll. implement some google logic.

--- Microsoft Project to Google Calendar/ConvertProjectToGCal.cs
+++ Microsoft Project to Google Calendar/ConvertProjectToGCal.cs
@@ -6,9 +6,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+
+//needed for MPXJ (to handle MS Project)
 using net.sf.mpxj;
 using net.sf.mpxj.mpp;
 using net.sf.mpxj.reader;
+
+//needed for Google Data API (to handle Google Calendar)
+using Google.GData.Calendar;
+using Google.GData.Client;
+using Google.GData.Extensions;
 
 namespace Microsoft_Project_to_Google_Calendar
 {
@@ -42,14 +49,16 @@ namespace Microsoft_Project_to_Google_Calendar
         {
             //read MS Project file
             ProjectFile projectFile = this.readProjectFile(this.textBoxFileName.Text);
+            Console.WriteLine("Step 1: Opened MS Project File \"" + this.textBoxFileName.Text + "\".");
             
             //get task list from open project file
             Task[] tasks = this.getAllProjectTasks(projectFile);
+            Console.WriteLine("Step 2: Returned " + tasks.Length + " tasks.");
 
             //debug: print all tasks found
             foreach (Task task in tasks)
             {
-                System.Diagnostics.Debug.WriteLine("Task found: " + task.getName());
+                Console.WriteLine(" >> Task by name \"" + task.getName() + "\" starting on \"" + task.getStart().toString() + "\" and ending on \"" + task.getStop().toString() + "\".");
             }
         }
 
@@ -58,7 +67,7 @@ namespace Microsoft_Project_to_Google_Calendar
         /// </summary>
         /// <param name="mppFile">MPP file to read.</param>
         /// <returns></returns>
-        public ProjectFile readProjectFile(String mppFile)
+        protected ProjectFile readProjectFile(String mppFile)
         {
             //get the reader
             ProjectReader reader = ProjectReaderUtility.getProjectReader(mppFile);
@@ -71,7 +80,7 @@ namespace Microsoft_Project_to_Google_Calendar
         /// </summary>
         /// <param name="projectFile">ProjectFile instance.</param>
         /// <returns>List of tasks.</returns>
-        public Task[] getAllProjectTasks(ProjectFile projectFile)
+        protected Task[] getAllProjectTasks(ProjectFile projectFile)
         {
             //get tasks
             java.util.List taskList = projectFile.getAllTasks();
@@ -97,6 +106,45 @@ namespace Microsoft_Project_to_Google_Calendar
             //then return the secondary list, otherwise permit a run-time exception
             //this is a wild bunch of magic anyways
             return allTasks;
+        }
+
+        /// <summary>
+        /// Translates a MS Project task into a Google Data API CalendarEntry for Google Calendar.
+        /// </summary>
+        /// <param name="task">MS Project task instance.</param>
+        /// <returns>A new EventEntry from Google Data API.</returns>
+        protected EventEntry translateProjectTaskToCalendarEntry(Task task)
+        {
+            //create EventEntry for Google API
+            EventEntry eventEntry = new EventEntry();
+
+            //create title, assign MS Project task name as title
+            AtomTextConstruct atomTitle = new AtomTextConstruct(AtomTextConstructElementType.Title, task.getName());
+
+            //assign title to EventEntry
+            eventEntry.Title = atomTitle;
+
+            //create When object for Google API
+            When dateTime = new When();
+
+            //write these down
+            java.util.Date taskStart = task.getStart();
+            java.util.Date taskStop = task.getStop();
+
+            //now convert them from Java to .Net and tell When what's up
+            dateTime.StartTime = new DateTime(taskStart.getYear(), taskStart.getMonth(), taskStart.getDate(), taskStart.getHours(), taskStart.getMinutes(), taskStart.getSeconds(), DateTimeKind.Local);
+            dateTime.EndTime = new DateTime(taskStop.getYear(), taskStop.getMonth(), taskStop.getDate(), taskStop.getHours(), taskStop.getMinutes(), taskStop.getSeconds(), DateTimeKind.Local);
+
+            //add the When to the event entry
+            eventEntry.Times.Add(dateTime);
+
+            //return the EventEntry
+            return eventEntry;
+        }
+
+        protected void updateGoogleCalendar()
+        {
+
         }
     }
 }
