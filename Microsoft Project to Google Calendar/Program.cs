06c2c93726d05689754d7cafc27b28ff45ae8bbf
David Ball <david@daball.me>
2011-04-01 02:35:32 -0400

    created empty c# project

---
+++ Microsoft Project to Google Calendar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Microsoft_Project_to_Google_Calendar
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
